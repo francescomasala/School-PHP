20b3880eec21e29204577c367b93debb03317a2a
Francesco Masala <mail@francescomasala.me>
2022-11-22 10:46:30 +0100

    Added and fixed exercises

--- Esercizi/tabella.php
+++ Esercizi/tabella.php
@@ -2,6 +2,7 @@
 <html lang="it-IT">
 
 <head>
+
     <!-- RIFERIMENTI GENERICI -->
     <meta charset="utf-8"/>
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
@@ -11,6 +12,7 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-italia@2.0.4/dist/css/bootstrap-italia.min.css"
           integrity="sha256-OAlsfaN58WYNlpH0uSz3xuJ8vrujFxoIlIFr8rySXqc=" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap-italia@2.0.4/dist/js/bootstrap-italia.bundle.min.js"></script>
+
 
 </head>
 <body>
@@ -62,8 +64,27 @@
                 <div class="card-wrapper card-space">
                     <div class="card card-bg shadow ">
                         <div class="card-body">
-                            <h1>Demo PHP</h1>
-                            <?php echo '<p>Hello World</p>'; ?>
+                            <?php
+                            $Titolo = 'prova';
+                            $Righe = 10;
+                            $Colonne = 10;
+
+                            echo("<h1> $Titolo </h1>");
+                            echo("<table>");
+                            for ($i = 0; $i < $Righe; $i++) {
+                                echo("<tr>");
+                                for ($x = 0; $x <= $Colonne; $x++) {
+                                    echo("<td>$i,$x</td>");
+                                }
+                                echo("</tr>");
+                            }
+                            echo("</table>");
+                            /*
+                            -------------
+                            |1,1|1,2|1,3|
+                            -------------
+                            */
+                            ?>
                         </div>
                     </div>
                 </div>
