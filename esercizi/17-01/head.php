712c744fa927bf9edf9ca8e9aa984af2fbf885a5
Francesco Masala <mail@francescomasala.me>
2023-01-17 08:50:13 +0100

    Added PHP Include and PHP File Upload

---
+++ esercizi/17-01/head.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="it-IT">
+
+<head>
+    <!-- RIFERIMENTI GENERICI -->
+    <meta charset="utf-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
+    <title>Test Include PHP</title>
+    <link rel="stylesheet" href="https://unpkg.com/@picocss/pico@latest/css/pico.min.css">
+</head>
---
+++ esercizi/17-01/index.php
@@ -0,0 +1,16 @@
+<?php include 'head.php'; ?>
+
+<body>
+<main class="container">
+    <h1> CSS Stylesheet iniettato tramite il file head.php</h1>
+    <div class="grid">
+        <div>
+            <form action="upload.php" method="post" enctype="multipart/form-data">
+                Carica un file JPG, JPEG, PNG o GIF:
+                <input type="file" name="fileToUpload" id="fileToUpload">
+                <input type="submit" value="Upload Image" name="submit">
+        </div>
+    </div>
+</main>
+</body>
+</html>
